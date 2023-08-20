5776b15e0b380c367b54749aa64627ca8e23ef55
TrongDungEmb98 <ntdungembedded98@gmail.com>
2023-08-20 21:38:05 +0700

    pull_all done

--- README.md
+++ README.md
@@ -3,7 +3,8 @@ buildroot + linux kernel + uboot supporting Goke gk7202v300 SoC
 
 
 # Flashing OpenIPC firmware
-sudo chmod 777 /dev/ttyUSB0 && ./burn --chip gk7202v300 --file=../u-boot-gk7202v300-universal.bin --break; picocom -b 115200 --databits 8 --parity n --stopbits 1 --flow n --logfile=log-$(date +%s).txt /dev/ttyUSB0
+sudo chmod 777 /dev/ttyUSB0 && ./burn --chip gk7202v300 --file=../u-boot-gk7202v300-universal.bin --break;
+picocom -b 115200 --databits 8 --parity n --stopbits 1 --flow n --logfile=log-$(date +%s).txt /dev/ttyUSB0
 setenv flashsize 0x800000
 mw.b ${baseaddr} 0xff ${flashsize}
 loady ${baseaddr}
--- build.sh
+++ build.sh
@@ -10,10 +10,11 @@
 
 echo "Build Script for Linux OS running on Goke GK7202V300 SoC"
 
-# Environment
+working_dir=$PWD
+
+# toolchain
 toolchain_dir="toolchain"
 cross_compiler="arm-linux-gnueabihf"
-working_dir=$PWD
 
 #uboot=========================================================
 u_boot_dir="uboot_source"
@@ -32,13 +33,6 @@ buildroot_config_file=""
 
 #pull===================================================================
 pull_toolchain() {
-	# CONFIG:= br-ext-chip-goke/configs/gk7202v300_lite_defconfig
-	# BR2_DEFCONFIG=$(PWD)/$(CONFIG) defconfig
-	# BOARD=gk7202v300_lite
-	# BR_VER = 2023.02.2
-	# BR2_EXTERNAL=$(PWD)/general
-	#BR2_TOOLCHAIN_EXTERNAL_URL="https://github.com/openipc/firmware/releases/download/$(OPENIPC_TOOLCHAIN).tgz"
-	# OPENIPC_TOOLCHAIN := latest/$(shell $(BR2_EXTERNAL)/scripts/show_toolchains.sh $(BR2_DEFCONFIG))
 	rm -rf ${working_dir}/${toolchain_dir}
 	mkdir -p ${working_dir}/${toolchain_dir}
 	cd ${working_dir}/${toolchain_dir}
@@ -55,7 +49,7 @@ pull_toolchain() {
 
 pull_uboot() {
 	rm -rf ${working_dir}/${u_boot_dir} &&
-		git clone  https://github.com/OpenIPC/u-boot-gk7205v200.git ${working_dir}/${u_boot_dir}
+		git clone https://github.com/OpenIPC/u-boot-gk7205v200.git ${working_dir}/${u_boot_dir}
 	if [ ! -d ${working_dir}/${u_boot_dir} ]; then
 		echo "Error:pull u_boot failed"
 		exit 0
@@ -79,11 +73,10 @@ pull_linux() {
 
 pull_buildroot() {
 	sudo rm -rf ${working_dir}/${buildroot_dir}
-	#wget https://buildroot.org/downloads/buildroot-2017.08.tar.gz && tar -xvf buildroot-2017.08.tar.gz
-	curl --continue-at - --output /tmp/buildroot_dl/buildroot-2023.02.1.tar.gz https://buildroot.org/downloads/buildroot-2023.02.1.tar.gz
-	git clone https://github.com/Unturned3/v3s_buildroot.git ${working_dir}/${buildroot_dir}
-
-	if [ ! -d ${working_dir}/${buildroot_dir} ]; then
+	mkdir -p ${working_dir}/${buildroot_dir}
+	cd ${working_dir}/${buildroot_dir}
+	wget https://buildroot.org/downloads/buildroot-2023.02.1.tar.gz && tar -xvf buildroot-2023.02.1.tar.gz
+	if [ ! -d ${working_dir}/${buildroot_dir}/buildroot-2023.02.1 ]; then
 		echo "Error:pull buildroot failed"
 		exit 0
 	else
@@ -286,179 +279,6 @@ build() {
 	copy_buildroot
 }
 
-#pack=========================================================
-pack_spiflash_normal_size_img() {
-	cd ${working_dir}
-
-	#rootfs
-	sudo rm -rf ${working_dir}/output/rootfs && mkdir -p ${working_dir}/output/rootfs
-	tar -C ${working_dir}/output/rootfs -xvf ${working_dir}/output/rootfs.tar >/dev/null 2>&1
-	sudo chown root ${working_dir}/output/rootfs/bin/* -R
-	sudo cp ${working_dir}/interfaces ${working_dir}/output/rootfs/etc/network/interfaces
-
-	#add config files
-	sudo cp ${working_dir}/interfaces ${working_dir}/output/rootfs/etc/network/interfaces &&
-		sudo cp ${working_dir}/service_ntp/S43hunonic_ntp ${working_dir}/output/rootfs/etc/init.d/ &&
-		sudo cp ${working_dir}/service_audio/S41hunonic_audio ${working_dir}/output/rootfs/etc/init.d/ &&
-		sudo cp ${working_dir}/service_audio/file_example_WAV_1MG.wav ${working_dir}/output/rootfs/root/ &&
-		sudo chown root ${working_dir}/output/rootfs/etc/init.d/S41hunonic_audio -R
-	sudo chmod 777 ${working_dir}/output/rootfs/etc/init.d/S41hunonic_audio
-
-	#add user app file
-	# sudo cp /home/dungnt98/hunonic_gateway_app/sources/manager/build_manager_service/manager_service \
-	# 		${working_dir}/output/rootfs/root/app
-	# sudo chown root ${working_dir}/output/rootfs/root/app -R
-	# sudo chmod 777 ${working_dir}/output/rootfs/root/app
-
-	#add wifi config
-	sudo cp ${working_dir}/service_wifi/wpa_supplicant.conf ${working_dir}/output/rootfs/etc/
-	sudo cp ${working_dir}/service_wifi/S42hunonic_wifi ${working_dir}/output/rootfs/etc/init.d/ &&
-		sudo chown root ${working_dir}/output/rootfs/etc/init.d/S42hunonic_wifi -R
-	sudo chmod 777 ${working_dir}/output/rootfs/etc/init.d/S42hunonic_wifi
-
-	sudo mkfs.jffs2 -s 0x100 -e 0x10000 -p 0x1AF0000 -d ${working_dir}/output/rootfs/ -o ${working_dir}/output/jffs2.img
-
-	ROOTFS_FILE=${working_dir}/output/jffs2.img
-	UBOOT_FILE=${working_dir}/output/${uboot_file}
-	DTB_FILE=${working_dir}/output/${dtb_file}
-	KERNEL_FILE=${working_dir}/output/zImage
-	OUT_FILENAME=${working_dir}/output/flashimg.bin
-
-	dd if=/dev/zero of=$OUT_FILENAME bs=1M count=16 #flash 16M
-	dd if=$UBOOT_FILE of=$OUT_FILENAME bs=1K conv=notrunc
-	dd if=$DTB_FILE of=$OUT_FILENAME bs=1K seek=1024 conv=notrunc
-	dd if=$KERNEL_FILE of=$OUT_FILENAME bs=1K seek=1088 conv=notrunc
-	dd if=$ROOTFS_FILE of=$OUT_FILENAME bs=1K seek=5184 conv=notrunc
-
-	echo "done"
-	#rm -rf ${working_dir}/output/rootfs ${working_dir}/output/jffs2.img
-}
-
-pack_tf_normal_size_img() {
-	_ROOTFS_FILE=${working_dir}/output/rootfs.tar.gz
-	_ROOTFS_SIZE=$(gzip -l $_ROOTFS_FILE | sed -n '2p' | awk '{print $2}')
-	_ROOTFS_SIZE=$(echo "scale=3;$_ROOTFS_SIZE/1024/1024" | bc)
-
-	_UBOOT_SIZE=1
-	_CFG_SIZEKB=0
-	_P1_SIZE=16
-	_IMG_SIZE=200
-	_kernel_mod_dir_name=$(ls ${working_dir}/output/modules/lib/modules/)
-	_MOD_FILE=${working_dir}/output/modules/lib/modules/${_kernel_mod_dir_name}
-	_MOD_SIZE=$(du $_MOD_FILE --max-depth=0 | cut -f 1)
-	_MOD_SIZE=$(echo "scale=3;$_MOD_SIZE/1024" | bc)
-	_MIN_SIZE=$(echo "scale=3;$_UBOOT_SIZE+$_P1_SIZE+$_ROOTFS_SIZE+$_MOD_SIZE+$_CFG_SIZEKB/1024" | bc) #+$_OVERLAY_SIZE
-	_MIN_SIZE=$(echo "$_MIN_SIZE" | bc)
-	echo "--->min img size = $_MIN_SIZE MB"
-	_MIN_SIZE=$(echo "${_MIN_SIZE%.*}+1" | bc)
-
-	_FREE_SIZE=$(echo "$_IMG_SIZE-$_MIN_SIZE" | bc)
-	_IMG_FILE=${working_dir}/output/image/lichee-zero-normal-size.img
-	mkdir -p ${working_dir}/output/image
-	rm $_IMG_FILE
-	dd if=/dev/zero of=$_IMG_FILE bs=1M count=$_IMG_SIZE
-	if [ $? -ne 0 ]; then
-		echo "getting error in creating dd img!"
-		exit
-	fi
-	_LOOP_DEV=$(sudo losetup -f)
-	if [ -z $_LOOP_DEV ]; then
-		echo "can not find a loop device!"
-		exit
-	fi
-	sudo losetup $_LOOP_DEV $_IMG_FILE
-	if [ $? -ne 0 ]; then
-		echo "dd img --> $_LOOP_DEV error!"
-		sudo losetup -d $_LOOP_DEV >/dev/null 2>&1 && exit
-	fi
-	echo "--->creating partitions for tf image ..."
-	# size only can be integer
-	cat <<EOT | sudo sfdisk $_IMG_FILE
-${_UBOOT_SIZE}M,${_P1_SIZE}M,c
-,,L
-EOT
-
-	sleep 2
-	sudo partx -u $_LOOP_DEV
-	sudo mkfs.vfat ${_LOOP_DEV}p1 || exit
-	sudo mkfs.ext4 ${_LOOP_DEV}p2 || exit
-	if [ $? -ne 0 ]; then
-		echo "error in creating partitions"
-		sudo losetup -d $_LOOP_DEV >/dev/null 2>&1 && exit
-	fi
-
-	#pack uboot
-	echo "--->writing u-boot-sunxi-with-spl to $_LOOP_DEV"
-	_UBOOT_FILE=${working_dir}/output/${uboot_file}
-	sudo dd if=$_UBOOT_FILE of=$_LOOP_DEV bs=1024 seek=8
-	if [ $? -ne 0 ]; then
-		echo "writing u-boot error!"
-		sudo losetup -d $_LOOP_DEV >/dev/null 2>&1 && exit
-	fi
-
-	sudo sync
-	mkdir -p ${working_dir}/output/p1 >/dev/null 2>&1
-	mkdir -p ${working_dir}/output/p2 >/dev/null 2>&1
-	sudo mount ${_LOOP_DEV}p1 ${working_dir}/output/p1
-	sudo mount ${_LOOP_DEV}p2 ${working_dir}/output/p2
-
-	echo "--->copy boot and rootfs files..."
-	sudo rm -rf ${working_dir}/output/p1/* && sudo rm -rf ${working_dir}/output/p2/*
-
-	#pack linux kernel
-	_KERNEL_FILE=${working_dir}/output/zImage
-	_DTB_FILE=${working_dir}/output/${dtb_file}
-	sudo cp $_UBOOT_FILE ${working_dir}/output/p1/${uboot_file} &&
-		sudo cp $_KERNEL_FILE ${working_dir}/output/p1/zImage &&
-		sudo cp $_DTB_FILE ${working_dir}/output/p1/ &&
-		sudo cp ${working_dir}/output/boot.scr ${working_dir}/output/p1/ &&
-		echo "--->p1 done~"
-
-	sudo tar xzvf $_ROOTFS_FILE -C ${working_dir}/output/p2/ >/dev/null 2>&1 &&
-		echo "--->p2 done~"
-	#kernel module
-	sudo mkdir -p ${working_dir}/output/p2/lib/modules/${_kernel_mod_dir_name}/ &&
-		sudo cp -r $_MOD_FILE/* ${working_dir}/output/p2/lib/modules/${_kernel_mod_dir_name}/ &&
-		echo "--->modules done~"
-
-	#add config files
-	sudo cp ${working_dir}/interfaces ${working_dir}/output/p2/etc/network/interfaces &&
-		sudo cp ${working_dir}/service_ntp/S43hunonic_ntp ${working_dir}/output/p2/etc/init.d/ &&
-		sudo cp ${working_dir}/service_audio/S41hunonic_audio ${working_dir}/output/p2/etc/init.d/ &&
-		sudo cp ${working_dir}/service_audio/file_example_WAV_1MG.wav ${working_dir}/output/p2/root/ &&
-		sudo chown root ${working_dir}/output/p2/etc/init.d/S41hunonic_audio -R
-	sudo chmod 777 ${working_dir}/output/p2/etc/init.d/S41hunonic_audio -R
-	sudo chown root ${working_dir}/output/p2/bin/* -R
-
-	#add user app file
-	# sudo cp /home/dungnt98/hunonic_gateway_app/sources/manager/build_manager_service/manager_service \
-	# 		${working_dir}/output/p2/root/app
-	# sudo chown root ${working_dir}/output/p2/root/app -R
-	# sudo chmod 777 ${working_dir}/output/p2/root/app
-
-	#add wifi config
-	sudo cp ${working_dir}/service_wifi/wpa_supplicant.conf ${working_dir}/output/p2/etc/
-	sudo cp ${working_dir}/service_wifi/S42hunonic_wifi ${working_dir}/output/p2/etc/init.d/ &&
-		sudo chown root ${working_dir}/output/p2/etc/init.d/S42hunonic_wifi -R
-	sudo chmod 777 ${working_dir}/output/p2/etc/init.d/S42hunonic_wifi -R
-
-	if [ $? -ne 0 ]; then
-		echo "copy files error! "
-		sudo losetup -d $_LOOP_DEV >/dev/null 2>&1
-		sudo umount ${_LOOP_DEV}p1 ${_LOOP_DEV}p2 >/dev/null 2>&1
-		exit
-	fi
-
-	echo "--->The tf card image-packing task done~"
-	sudo sync
-	sleep 2
-	sudo umount ${working_dir}/output/p1 ${working_dir}/output/p2 && sudo losetup -d $_LOOP_DEV
-	if [ $? -ne 0 ]; then
-		echo "umount or losetup -d error!!"
-		exit
-	fi
-}
-
 #=======================MAIN============================
 if [ "${1}" = "" ] && [ ! "${1}" = "build_tf" ] && [ ! "${1}" = "build_flash" ] && [ ! "${1}" = "pull_all" ]; then
 	echo "Usage: script.sh [build_flash | build_tf | pull_all | clean]"；
@@ -468,11 +288,6 @@ if [ "${1}" = "" ] && [ ! "${1}" = "build_tf" ] && [ ! "${1}" = "build_flash" ] 
 	echo "build_tf          Build zero firmware booted from tf"
 	echo "pull_all         Pull build env from internet"
 	echo "clean            Clean build env"
-	exit 0
-fi
-
-if [ ! -f ${working_dir}/script.sh ]; then
-	echo "Error:Please enter packge root dir"
 	exit 0
 fi
 
